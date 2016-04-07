d97da6a3eaccddd93094a2c13d8345f6963c440c
mnemotiv <hostmaster@nodetemple.com>
2016-04-07 15:12:35 +0300

    Simplify kube-down

--- kube-down.sh
+++ kube-down.sh
@@ -2,19 +2,12 @@
 
 echo "Starting to teardown Kubernetes..."
 
-echo "- Removing replication controllers, pods, services, endpoints and namespaces..."
-kubectl delete --grace-period=1 rc,po,svc,ep,ns --all &>/dev/null
-
-echo "- Removing main Kubernetes containers..."
-docker-compose kill &>/dev/null
-docker-compose rm -fv &>/dev/null
-
 K8S_CONTAINERS=$(docker ps -a -f "name=k8s_" -q)
-echo "- Removing all other containers that were started by Kubernetes..."
+echo "- Removing all Kubernetes containers..."
 docker kill ${K8S_CONTAINERS} &>/dev/null
 docker rm -fv ${K8S_CONTAINERS} &>/dev/null
 
 echo "- Removing manifests and other data..."
 sudo rm -rf /etc/kubernetes/manifests /var/lib/kubelet
 
-echo "Done: Kubernetes is down."
+echo "Kubernetes is down."
