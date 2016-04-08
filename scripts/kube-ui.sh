8b83a438535be63f27ebf588f4ee05c902b86334
mnemotiv <hostmaster@nodetemple.com>
2016-04-08 15:04:08 +0300

    Move UI container to manifest

--- scripts/kube-ui.sh
+++ scripts/kube-ui.sh
@@ -5,65 +5,22 @@ kubectl create -f - << EOF
 kind: List
 apiVersion: v1
 items:
-- kind: ReplicationController
-  apiVersion: v1
-  metadata:
-    name: k8s-dashboard
-    namespace: kube-system
-    labels:
-      app: k8s-dashboard
-  spec:
-    replicas: 1
-    selector:
-      app: k8s-dashboard
-    template:
-      metadata:
-        labels:
-          app: k8s-dashboard
-      spec:
-        hostNetwork: false
-        restartPolicy: Always
-        containers:
-        - name: k8s-dashboard
-          image: gcr.io/google_containers/kubernetes-dashboard-amd64:canary
-          imagePullPolicy: Always
-          ports:
-          - containerPort: 9090
-            protocol: TCP
-          args:
-          - "--apiserver-host=http://127.0.0.1:8080"
 - kind: Service
   apiVersion: v1
   metadata:
     name: dashboard
-    namespace: kube-system
+    namespace: default
     labels:
-      app: k8s-dashboard
+      app: k8s-api
       kubernetes.io/name: Kube-UI
       kubernetes.io/cluster-service: "true"
   spec:
     selector:
-      app: k8s-dashboard
+      app: k8s-api
     type: NodePort
     ports:
     - nodePort: 9090
       port: 9090
       targetPort: 9090
-      protocol: TCP
-- kind: Service
-  apiVersion: v1
-  metadata:
-    name: api
-    namespace: default
-    labels:
-      app: k8s-api
-      kubernetes.io/name: Kube-API
-      kubernetes.io/cluster-service: "true"
-  spec:
-    selector:
-      app: k8s-api
-    ports:
-    - port: 9091
-      targetPort: 8080
       protocol: TCP
 EOF
