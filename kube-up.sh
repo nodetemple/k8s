a3ce1e5b44c5b214151b7e821695609cc5511f01
mnemotiv <hostmaster@nodetemple.com>
2016-04-11 15:46:44 +0300

    Refactor stack

--- kube-up.sh
+++ kube-up.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-COMPOSE_VERSION=1.6.2
-K8S_VERSION=1.2.0
+export K8S_VERSION=1.2.2
+
+export SERVICE_IP_RANGE=10.0.0.0/24
+export K8S_SERVICE_IP=10.0.0.1
+export DNS_SERVICE_IP=10.0.0.10
+export DNS_HOST=cluster.local
+export POD_NETWORK=10.1.0.0/16
+
+export PUBLIC_IP=$(awk -F= '/COREOS_PUBLIC_IPV4/ {print $2}' /etc/environment)
 
 echo "Initializing Kubernetes v${K8S_VERSION} stack..."
 
-if ! [ "$(command -v docker-compose)" ]; then
-  echo "- Installing docker-compose..."
-  curl -Ls https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-Linux-x86_64 > docker-compose
-  chmod +x docker-compose
-  sudo mkdir -p /opt/bin
-  sudo mv -f docker-compose /opt/bin/docker-compose
-fi
-
+# TODO: update kubectl if version changed
 if ! [ "$(command -v kubectl)" ]; then
   echo "- Installing kubectl..."
   curl -Ls http://storage.googleapis.com/kubernetes-release/release/v${K8S_VERSION}/bin/linux/amd64/kubectl > kubectl
@@ -24,6 +24,43 @@ echo "- Configuring kubectl..."
 kubectl config set-cluster k8s --server=http://127.0.0.1:8080
 kubectl config set-context k8s --cluster=k8s
 kubectl config use-context k8s
+
+echo "- Configuring flannel..."
+sudo systemctl stop docker.service
+
+    local TEMPLATE=/etc/flannel/options.env
+    rm -rf $TEMPLATE
+    [ -f $TEMPLATE ] || {
+        echo "TEMPLATE: $TEMPLATE"
+        mkdir -p $(dirname $TEMPLATE)
+        cat << EOF > $TEMPLATE
+FLANNELD_IFACE=$ADVERTISE_IP
+FLANNELD_ETCD_ENDPOINTS=$ETCD_ENDPOINTS
+EOF
+    }
+
+    local TEMPLATE=/etc/systemd/system/flanneld.service.d/40-ExecStartPre-symlink.conf
+    rm -rf $TEMPLATE
+    [ -f $TEMPLATE ] || {
+        echo "TEMPLATE: $TEMPLATE"
+        mkdir -p $(dirname $TEMPLATE)
+        cat << EOF > $TEMPLATE
+[Service]
+ExecStartPre=/usr/bin/ln -sf /etc/flannel/options.env /run/flannel/options.env
+EOF
+    }
+
+    local TEMPLATE=/etc/systemd/system/docker.service.d/40-flannel.conf
+    rm -rf $TEMPLATE
+    [ -f $TEMPLATE ] || {
+        echo "TEMPLATE: $TEMPLATE"
+        mkdir -p $(dirname $TEMPLATE)
+        cat << EOF > $TEMPLATE
+[Unit]
+Requires=flanneld.service
+After=flanneld.service
+EOF
+    }
 
 echo "- Enabling Docker engine..."
 sudo systemctl enable docker.service
@@ -40,13 +77,29 @@ if kubectl cluster-info &>/dev/null; then
 fi
 
 echo "- Starting main Kubenetes containers..."
-docker-compose up -d
+docker run -d --name=k8s_kubelet --net=host --pid=host --privileged \
+  --volume=/:/rootfs:ro \
+  --volume=/sys:/sys:ro \
+  --volume=/dev:/dev \
+  --volume=/var/lib/docker/:/var/lib/docker:rw \
+  --volume=/var/lib/kubelet/:/var/lib/kubelet:rw \
+  --volume=/var/run:/var/run:rw \
+  --volume=${PWD}/manifests/etc:/etc/kubernetes/manifests:ro \
+  --volume=${PWD}/manifests/srv:/srv/kubernetes/manifests:ro \
+  gcr.io/google_containers/hyperkube:v${K8S_VERSION} \
+  /hyperkube kubelet \
+    --containerized --enable-server --allow-privileged --config=/etc/kubernetes/manifests \
+    --address=0.0.0.0 --hostname-override=${PUBLIC_IP} --api-servers=http://127.0.0.1:8080 \
+    --cluster-dns=${DNS_SERVICE_IP} --cluster-domain=${DNS_HOST} \
+    --read-only-port=0 --cadvisor-port=0
 
 echo "- Waiting for Kubernetes stack to become available..."
 until kubectl cluster-info &>/dev/null; do
   sleep 1
 done
 
-kubectl create -f addons
+#curl --silent -X PUT -d "value={\"Network\":\"$POD_NETWORK\",\"Backend\":{\"Type\":\"vxlan\"}}" "$ACTIVE_ETCD/v2/keys/coreos.com/network/config?prevExist=false"
+
+#kubectl create -f addons
 
 echo "Kubernetes stack is up."
